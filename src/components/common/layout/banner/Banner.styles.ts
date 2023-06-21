cdf2531f1469378d0d2601768075433f30c80400
jeonpilseong <abcabc3502@gmail.com>
2023-06-22 05:18:07 +0900

    UPDATE: 캐러셀 이미지 수정

--- src/components/common/layout/banner/Banner.styles.ts
+++ src/components/common/layout/banner/Banner.styles.ts
@@ -2,21 +2,24 @@ import styled from '@emotion/styled'
 
 export const Wrapper = styled.div`
   margin: auto;
-  width: 60%;
+  width: 100%;
   margin-top: 4rem;
 `
 
 export const Img1 = styled.div`
-  height: 40rem;
+  height: 60rem;
   background-image: url('/images/carousel/banner5.jpg');
+  background-size: cover;
 `
 
 export const Img2 = styled.div`
-  height: 40rem;
-  background-image: url('/images/carousel/banner4.jpg');
+  height: 60rem;
+  background-image: url('/images/carousel/banner1.jpg');
+  background-size: cover;
 `
 
 export const Img3 = styled.div`
-  height: 40rem;
+  height: 60rem;
   background-image: url('/images/carousel/banner3.jpg');
+  background-size: cover;
 `
